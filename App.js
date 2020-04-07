5a6a973f9414e571c81514ecaebb31ff5f9ae7ae
Sal Tamay <sal.tamay@outlook.com>
2020-04-06 23:35:27 -0400

    feat: Add input field and Add goals button

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [text, setText] = useState(
-    'Open up App.js to start working on your app!!!'
-  );
   return (
-    <View>
-      <View></View>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Course Goal' style={styles.inputElement} />
+        <Button title='Add' style={styles.addButton} />
+      </View>
       <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    margin: 30,
+    marginTop: 50,
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  inputElement: {
+    borderBottomColor: 'rgba(0, 0, 0, 0.5)',
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    marginRight: 20,
+    padding: 10,
+    width: '80%',
+  },
+  addButton: {
+    width: '18%',
+  },
+});
